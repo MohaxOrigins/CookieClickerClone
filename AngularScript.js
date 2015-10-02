e498b715db0b2aa9209ff94c757b837239131b42
unknown <mohaxorigins@gmail.com>
2015-10-02 23:53:27 +0300

    Finished V.0.2.1

--- AngularScript.js
+++ AngularScript.js
@@ -1,9 +1,5 @@
-var app= angular.module("myApp",['angular-growl']);
-app.controller('cont',['$scope', 'growl', function($scope, growl){
-    //TESTING NOTIFS
-    $scope.Notif=function(){
-        growl.info("trolling you");
-    };
+var app= angular.module("myApp",[]);
+app.controller('cont',['$scope' , function($scope, growl){
     //Active tab
     $scope.WorkA="active";
     $scope.HousesA="";
@@ -11,7 +7,7 @@ app.controller('cont',['$scope', 'growl', function($scope, growl){
     $scope.TransportA="";
     $scope.EatA="";
     $scope.MiscA="";
-    //Tab Switcher
+//Tab Switcher
     $scope.MiscAc=function(){
         $scope.HousesA="";
         $scope.JobsA="";
@@ -60,19 +56,17 @@ app.controller('cont',['$scope', 'growl', function($scope, growl){
         $scope.WorkA="";
         $scope.MiscA="";
     };
+    //TESTING NOTIFS
+    $scope.Notifications=[];
     //Board message
     $scope.Msg="Welcome to this game";
-    //Random messages
-    $scope.Meds="You find some tablets";
-    $scope.Food=["","You find an old sandwich","Someone threw a banana at you","You find some pet food","You return with a mini piece of cake","You find some chewing gum"];
-    $scope.Search=["","Sweet!, you found some bottles!","You find bottles behind the bench","You find some bottles in the trash can","That car just dropped some bottles!","A stranger gives you some bottles"]
     //Work actions
     $scope.WorkActions=function(workid){
         var seed = Math.floor((Math.random() * 5) + 1);
         switch(workid){
-            /** Search for bottles **/case 1: $scope.Msg=$scope.Search[seed];$scope.Prestige+=seed*2;  $scope.Bottles+=seed; if($scope.Mood>0){$scope.Mood-=seed*2;} if($scope.Hunger>0){$scope.Hunger-=seed*3}; break;
+            /** Search for bottles **/case 1: $scope.Msg=Search[seed];$scope.Prestige+=seed*2;  $scope.Bottles+=seed; if($scope.Mood>0){$scope.Mood-=seed*2;} if($scope.Hunger>0){$scope.Hunger-=seed*3}; break;
             /** Sell bottles **/case 2: if($scope.Hunger>0){$scope.Hunger-=seed*2};$scope.Msg="You sold "+$scope.Bottles+" Bottles for "+$scope.BottlePrice+" $ each, making in total "+$scope.Bottles*$scope.BottlePrice+"$";$scope.Prestige+=Math.floor($scope.Bottles/10); $scope.Money+= $scope.Bottles*$scope.BottlePrice;$scope.Mood+=Math.floor($scope.Bottles*$scope.BottlePrice*2); $scope.Bottles=0; break;
-            /** Search for food **/case 3: $scope.Msg=$scope.Food[seed];$scope.Hunger+=seed; if($scope.Mood>0){$scope.Mood-=seed*2}; if($scope.Health>0){$scope.Health-=Math.floor(seed)};break;
+            /** Search for food **/case 3: $scope.Msg=Food[seed];$scope.Hunger+=seed; if($scope.Mood>0){$scope.Mood-=seed*2}; if($scope.Health>0){$scope.Health-=Math.floor(seed)};break;
             /** Rest In the Park**/case 4: $scope.Mood+=seed*2; $scope.Hunger-=seed;break;
             /** Search for Meds**/case 5: $scope.Msg=$scope.Meds;$scope.Health+=seed*2;if($scope.Mood>0){$scope.Mood-=seed*2;}; if($scope.Hunger>0){$scope.Hunger-=seed*3}; break;
         }
@@ -106,6 +100,9 @@ app.controller('cont',['$scope', 'growl', function($scope, growl){
     $scope.Prestige=0;
     //Turn Routine
     $scope.Check=function(){
+        $scope.MoodShow=false;
+        $scope.HealthShow=false;
+        $scope.HungerShow=false;
         if($scope.Mood<0){$scope.Mood=0}
         if($scope.Hunger<0){$scope.Hunger=0}
         if($scope.Health<0){$scope.Health=0}
@@ -114,41 +111,75 @@ app.controller('cont',['$scope', 'growl', function($scope, growl){
         if($scope.Health>100){$scope.Health=100}
         $scope.BottlePrice= Math.floor(Math.random()*100 +1)/100;
         $scope.Turn+=1;
-        if($scope.Turn%365==0){
-            $scope.Age+=1;
+        if($scope.Turn%365==0) {
+            $scope.Age += 1;
         }
+        //Mood Check
         if($scope.Mood>70){
             $scope.MoodBar="progress-bar-success";
+            $scope.MoodCritical=0;
         } else if($scope.Mood>50){
             $scope.MoodBar="progress-bar-info";
+            $scope.MoodCritical=0;
         }else if($scope.Mood>30){
             $scope.MoodBar="progress-bar-warning";
+            $scope.MoodCritical=0;
         }else{
+            $scope.MoodCritical+=1;
+            $scope.MoodShow=true;
             $scope.MoodBar="progress-bar-danger";
         }
+        //Hunger Check
         if($scope.Hunger>70){
             $scope.HungerBar="progress-bar-success";
+            $scope.HungerCritical=0;
         } else if($scope.Hunger>50){
             $scope.HungerBar="progress-bar-info";
+            $scope.HungerCritical=0;
         }else if($scope.Hunger>30){
             $scope.HungerBar="progress-bar-warning";
+            $scope.HungerCritical=0;
         }else{
+            $scope.HungerCritical+=1;
             $scope.HungerBar="progress-bar-danger";
+            $scope.HungerShow=true;
         }
+        //Health Check
         if($scope.Health>70){
             $scope.HealthBar="progress-bar-success";
+            $scope.HealthCritical=0;
         } else if($scope.Health>50){
             $scope.HealthBar="progress-bar-info";
+            $scope.HealthCritical=0;
         }else if($scope.Health>30){
             $scope.HealthBar="progress-bar-warning";
+            $scope.HealthCritical=0;
         }else{
+            $scope.HealthCritical+=1;
+            $scope.HealthShow=true;
             $scope.HealthBar="progress-bar-danger";
         }
+        if($scope.HealthCritical>=7||$scope.HungerCritical>=7||$scope.MoodCritical>=7){
+            $scope.dead();
+        }
+        for(notif in $scope.Notifications){
+            var index = $scope.Notifications.indexOf(notif);
+            if (index > -1) {
+                $scope.Notifications.splice(index, 1);
+            }
+        }
     };
+    $scope.Works=WorkList;
     //Game Over
-    $scope.dead = function(){alert("Game Over, you died at Turn "+$scope.Turn);};
+    $scope.dead = function(){
+        var reasonOfDeath=null;
+        if($scope.HungerCritical>=7){reasonOfDeath="Hunger"};
+        if($scope.MoodCritical>=7){reasonOfDeath="Mood"};
+        if($scope.HealthCritical>=7){reasonOfDeath="Health"};
+        alert("Game Over, you died at Turn "+$scope.Turn+" reason of Death: "+reasonOfDeath);
+    };
     //Version
-    $scope.Ver="0.2.1/0.2.2 A";
+    $scope.Ver="0.2.1 CA";
     $scope.Title="Life sim "+$scope.Ver;
     //Setup
     $scope.HealthBar="progress-bar-success";
